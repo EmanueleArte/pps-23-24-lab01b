09fd1d4469a10f94b3db67f6a9ad8687a0d5a113
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-03-04 11:28:59 +0100

    Add test for overlapped positions

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -38,6 +38,23 @@ public class LogicTest {
     }
 
     @Test
+    public void testOverlappedPositioning() {
+        this.logics = new LogicsImpl(SIZE);
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (this.logics.hasPawn(i, j)) {
+                    int finalI = i;
+                    int finalJ = j;
+                    assertAll(
+                            () -> assertTrue(this.logics.hasPawn(finalI, finalJ)),
+                            () -> assertFalse(this.logics.hasKnight(finalI, finalJ))
+                    );
+                }
+            }
+        }
+    }
+
+    @Test
     public void testKnightImpossibleMove() {
         this.logics = new LogicsImpl(SIZE, new Pair<>(1, 1), new Pair<>(2, 1));
         assertFalse(this.logics.hit(1, 1));
