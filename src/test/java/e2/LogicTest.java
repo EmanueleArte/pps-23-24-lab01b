fe6f0d134b8504503414eefd5072bb2fee2d043b
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-03-07 21:07:37 +0100

    Create logic test class

---
+++ src/test/java/e2/LogicTest.java
@@ -0,0 +1,20 @@
+package e2;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LogicTest {
+
+    private static final int SIZE = 5;
+    private static final int MINES = 5;
+    private Logics logics;
+
+    @BeforeEach
+    void beforeEach() {
+        this.logics = new LogicsImpl(SIZE, MINES);
+    }
+
+
+
+}
